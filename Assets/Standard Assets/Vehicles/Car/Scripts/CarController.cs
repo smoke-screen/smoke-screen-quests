103fce8626e724676752a1c2e68da317dd162b33
Burton <burtonkeck@outlook.com>
2018-11-04 21:30:32 -0500

    made water slow you down and if you hit the barrier you cannot collect the chocolate

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -50,12 +50,13 @@ namespace UnityStandardAssets.Vehicles.Car
         private bool quest1 = false;
         private bool quest2 = false;
         private bool quest3 = false;
+        private bool lose = true;
 
         public bool Skidding { get; private set; }
         public float BrakeInput { get; private set; }
-        public float CurrentSteerAngle{ get { return m_SteerAngle; }}
-        public float CurrentSpeed{ get { return m_Rigidbody.velocity.magnitude*2.23693629f; }}
-        public float MaxSpeed{get { return m_Topspeed; }}
+        public float CurrentSteerAngle { get { return m_SteerAngle; } }
+        public float CurrentSpeed { get { return m_Rigidbody.velocity.magnitude * 2.23693629f; } }
+        public float MaxSpeed { get { return m_Topspeed; } }
         public float Revs { get; private set; }
         public float AccelInput { get; private set; }
         public Text firetext;
@@ -73,16 +74,16 @@ namespace UnityStandardAssets.Vehicles.Car
             m_MaxHandbrakeTorque = float.MaxValue;
 
             m_Rigidbody = GetComponent<Rigidbody>();
-            m_CurrentTorque = m_FullTorqueOverAllWheels - (m_TractionControl*m_FullTorqueOverAllWheels);
+            m_CurrentTorque = m_FullTorqueOverAllWheels - (m_TractionControl * m_FullTorqueOverAllWheels);
             firetext.text = "";
         }
 
 
         private void GearChanging()
         {
-            float f = Mathf.Abs(CurrentSpeed/MaxSpeed);
-            float upgearlimit = (1/(float) NoOfGears)*(m_GearNum + 1);
-            float downgearlimit = (1/(float) NoOfGears)*m_GearNum;
+            float f = Mathf.Abs(CurrentSpeed / MaxSpeed);
+            float upgearlimit = (1 / (float)NoOfGears) * (m_GearNum + 1);
+            float downgearlimit = (1 / (float)NoOfGears) * m_GearNum;
 
             if (m_GearNum > 0 && f < downgearlimit)
             {
@@ -99,24 +100,24 @@ namespace UnityStandardAssets.Vehicles.Car
         // simple function to add a curved bias towards 1 for a value in the 0-1 range
         private static float CurveFactor(float factor)
         {
-            return 1 - (1 - factor)*(1 - factor);
+            return 1 - (1 - factor) * (1 - factor);
         }
 
 
         // unclamped version of Lerp, to allow value to exceed the from-to range
         private static float ULerp(float from, float to, float value)
         {
-            return (1.0f - value)*from + value*to;
+            return (1.0f - value) * from + value * to;
         }
 
 
         private void CalculateGearFactor()
         {
-            float f = (1/(float) NoOfGears);
+            float f = (1 / (float)NoOfGears);
             // gear factor is a normalised representation of the current speed within the current gear's range of speeds.
             // We smooth towards the 'target' gear factor, so that revs don't instantly snap up or down when changing gear.
-            var targetGearFactor = Mathf.InverseLerp(f*m_GearNum, f*(m_GearNum + 1), Mathf.Abs(CurrentSpeed/MaxSpeed));
-            m_GearFactor = Mathf.Lerp(m_GearFactor, targetGearFactor, Time.deltaTime*5f);
+            var targetGearFactor = Mathf.InverseLerp(f * m_GearNum, f * (m_GearNum + 1), Mathf.Abs(CurrentSpeed / MaxSpeed));
+            m_GearFactor = Mathf.Lerp(m_GearFactor, targetGearFactor, Time.deltaTime * 5f);
         }
 
 
@@ -125,7 +126,7 @@ namespace UnityStandardAssets.Vehicles.Car
             // calculate engine revs (for display / sound)
             // (this is done in retrospect - revs are not used in force/power calculations)
             CalculateGearFactor();
-            var gearNumFactor = m_GearNum/(float) NoOfGears;
+            var gearNumFactor = m_GearNum / (float)NoOfGears;
             var revsRangeMin = ULerp(0f, m_RevRangeBoundary, CurveFactor(gearNumFactor));
             var revsRangeMax = ULerp(m_RevRangeBoundary, 1f, gearNumFactor);
             Revs = ULerp(revsRangeMin, revsRangeMax, m_GearFactor);
@@ -146,12 +147,12 @@ namespace UnityStandardAssets.Vehicles.Car
             //clamp input values
             steering = Mathf.Clamp(steering, -1, 1);
             AccelInput = accel = Mathf.Clamp(accel, 0, 1);
-            BrakeInput = footbrake = -1*Mathf.Clamp(footbrake, -1, 0);
+            BrakeInput = footbrake = -1 * Mathf.Clamp(footbrake, -1, 0);
             handbrake = Mathf.Clamp(handbrake, 0, 1);
 
             //Set the steer on the front wheels.
             //Assuming that wheels 0 and 1 are the front wheels.
-            m_SteerAngle = steering*m_MaximumSteerAngle;
+            m_SteerAngle = steering * m_MaximumSteerAngle;
             m_WheelColliders[0].steerAngle = m_SteerAngle;
             m_WheelColliders[1].steerAngle = m_SteerAngle;
 
@@ -163,7 +164,7 @@ namespace UnityStandardAssets.Vehicles.Car
             //Assuming that wheels 2 and 3 are the rear wheels.
             if (handbrake > 0f)
             {
-                var hbTorque = handbrake*m_MaxHandbrakeTorque;
+                var hbTorque = handbrake * m_MaxHandbrakeTorque;
                 m_WheelColliders[2].brakeTorque = hbTorque;
                 m_WheelColliders[3].brakeTorque = hbTorque;
             }
@@ -219,22 +220,31 @@ namespace UnityStandardAssets.Vehicles.Car
                 other.gameObject.SetActive(false);
                 firetext.text = ("Level 4!");
             }
+            else if (other.gameObject.CompareTag("Reset"))
+            {
+                m_Topspeed = 280;
+            }
 
             else if (other.gameObject.CompareTag("Chocolate"))
             {
-                if (quest1 && quest2 && quest3)
+                if (quest1 && quest2 && quest3 && lose)
                 {
                     other.gameObject.SetActive(false);
                     firetext.text = ("You Win!");
                 }
             }
+
             else if (other.gameObject.CompareTag("Lose"))
             {
                 {
+                    lose = false;
                     firetext.text = ("You Lose!");
                 }
             }
-        }
+            else if (other.gameObject.CompareTag("Water"))
+                m_Topspeed = 10;
+        
+    }
 
 
         private void ApplyDrive(float accel, float footbrake)
